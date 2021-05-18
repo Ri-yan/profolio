c8c71ceed808321edf27d980001f1c5ffef8a408
Ri-yan <Ri-yan@github.com>
2021-05-18 17:30:36 +0530

    Updates

---
+++ precache-manifest.b1d037495c9683ae29a525c33209f2ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f94ecfae014a83cb6ce",
+    "url": "/profolio/static/css/main.3db5de9c.chunk.css"
+  },
+  {
+    "revision": "1f94ecfae014a83cb6ce",
+    "url": "/profolio/static/js/main.b9a7dfd8.chunk.js"
+  },
+  {
+    "revision": "b0bc7d032673c7e57006",
+    "url": "/profolio/static/js/runtime~main.decb93ba.js"
+  },
+  {
+    "revision": "03b8c11c90cf60d9e166",
+    "url": "/profolio/static/css/2.25e103a1.chunk.css"
+  },
+  {
+    "revision": "03b8c11c90cf60d9e166",
+    "url": "/profolio/static/js/2.9e12874e.chunk.js"
+  },
+  {
+    "revision": "f2c121df5bc05f03f3ea",
+    "url": "/profolio/static/js/3.b1248334.chunk.js"
+  },
+  {
+    "revision": "e6b255d4102892e9728bdfcaa44b54c8",
+    "url": "/profolio/index.html"
+  }
+];
